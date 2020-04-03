3c02d94c244e88edeb16e6d028473b21eed37e1d
ghostxbh <ghostxbh@hotmail.com>
2020-04-03 23:26:32 +0800

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * @author ghostxbh
+ * @date 2020/4/3
+ * @description
+ */
+const IpProcess = require('./lib/ip_process');
+const UriProcess = require('./lib/uri_process');
+
+module.exports = {
+    IpProcess,
+    UriProcess
+};
